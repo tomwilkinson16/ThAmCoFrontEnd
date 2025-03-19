04313cd5f2ecffd992bf2d05bc68a5ce039de712
tomwilkinson16 <tomwilkinson622@gmail.com>
2025-03-19 23:26:32 +0000

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ThAmCoFrontEnd.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
